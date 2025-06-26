284e26088b2d80b0156f568e09f618a51eb216c2
Chase Balmer <chase.balmer@outlook.com>
2025-06-25 21:19:17 -0400

    Training Program Generator

--- Services/TrainingProgramService.cs
+++ Services/TrainingProgramService.cs
@@ -68,7 +68,7 @@ public class TrainingProgramService : ITrainingProgramService
         var userGuid = Guid.Parse(user.Id);
         var startDate = request.StartDate;
         var endDate = request.EndDate;
-        
+
 
         var trainingProgram = new TrainingProgram
         {
@@ -98,15 +98,15 @@ public class TrainingProgramService : ITrainingProgramService
         }
 
         trainingProgram.Title = request.Title;
-       
+
         trainingProgram.StartDate = request.StartDate;
-    
-    
+
+
         trainingProgram.EndDate = request.EndDate;
-    
-    
+
+
         trainingProgram.Tags = request.Tags;
-        
+
 
         await _context.SaveChangesAsync();
         return Result<TrainingProgramDTO>.Success(trainingProgram.ToDTO());
@@ -127,6 +127,85 @@ public class TrainingProgramService : ITrainingProgramService
         _context.TrainingPrograms.Remove(trainingProgram);
         await _context.SaveChangesAsync();
         return Result.NoContent();
+    }
+    
+    [McpServerTool, Description("Generate a populated training program with sessions and movements.")]
+    public async Task<Result<TrainingProgramDTO>> GeneratePopulatedTrainingProgramAsync(IdentityUser user, GeneratePopulatedTrainingProgramRequest request)
+    {
+        var userGuid = Guid.Parse(user.Id);
+        var trainingProgram = new TrainingProgram
+        {
+            TrainingProgramID = Guid.NewGuid(),
+            UserID = userGuid,
+            Title = request.TrainingProgram.Title,
+            StartDate = request.TrainingProgram.StartDate,
+            EndDate = request.TrainingProgram.EndDate,
+            Tags = request.TrainingProgram.Tags ?? []
+        };
+
+        _context.TrainingPrograms.Add(trainingProgram);
+        await _context.SaveChangesAsync();
+
+        foreach (var sessionRequest in request.GenerateTrainingSessionsRequests)
+        {
+            var trainingSession = new TrainingSession
+            {
+                TrainingSessionID = Guid.NewGuid(),
+                TrainingProgramID = trainingProgram.TrainingProgramID,
+                Date = sessionRequest.createTrainingSessionRequest.Date,
+                Status = TrainingSessionStatus.Planned         
+            };
+
+            _context.TrainingSessions.Add(trainingSession);
+            await _context.SaveChangesAsync();
+
+            int ordering = 0;
+            foreach (var movementRequest in sessionRequest.CreateMovementRequests)
+            {
+                var movementBase = await _context.MovementBases
+                    .FindAsync(movementRequest.MovementBaseID);
+
+                if (movementBase is null)
+                {
+                    return Result<TrainingProgramDTO>.Error($"Movement Base with ID:'{movementRequest.MovementBaseID}' not found.");
+                }
+
+                var movement = new Movement
+                {
+                    MovementID = Guid.NewGuid(),
+                    TrainingSessionID = trainingSession.TrainingSessionID,
+                    MovementBaseID = movementBase.MovementBaseID,
+                    Notes = movementRequest.Notes,
+                    Ordering = ordering
+                };
+
+                _context.Movements.Add(movement);
+                await _context.SaveChangesAsync();
+
+                foreach (var setEntryRequest in sessionRequest.CreateSetEntryRequests)
+                {
+                    var setEntry = new SetEntry
+                    {
+                        SetEntryID = Guid.NewGuid(),
+                        MovementID = movement.MovementID,
+                        ActualReps = setEntryRequest.ActualReps,
+                        ActualWeight = setEntryRequest.ActualWeight,
+                        ActualRPE = setEntryRequest.ActualRPE,
+                        RecommendedReps = setEntryRequest.RecommendedReps,
+                        RecommendedWeight = setEntryRequest.RecommendedWeight,
+                        RecommendedRPE = setEntryRequest.RecommendedRPE,
+                        WeightUnit = setEntryRequest.WeightUnit
+                    };
+
+                    _context.SetEntries.Add(setEntry);
+                }
+
+                ordering++;
+            }
+        }
+
+        await _context.SaveChangesAsync();
+        return Result<TrainingProgramDTO>.Success(trainingProgram.ToDTO());
     }
 
 
