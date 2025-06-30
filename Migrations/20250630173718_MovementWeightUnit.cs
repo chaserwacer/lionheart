3fa04abd49f41bd9aa4469fa4dbb1d9a22f0ba24
Chase Balmer <chase.balmer@outlook.com>
2025-06-30 13:42:57 -0400

    Movement Weight Unit migration

---
+++ Migrations/20250630173718_MovementWeightUnit.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace lionheart.Migrations
+{
+    /// <inheritdoc />
+    public partial class MovementWeightUnit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "WeightUnit",
+                table: "SetEntries");
+
+            migrationBuilder.AddColumn<int>(
+                name: "WeightUnit",
+                table: "Movements",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "WeightUnit",
+                table: "Movements");
+
+            migrationBuilder.AddColumn<int>(
+                name: "WeightUnit",
+                table: "SetEntries",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
